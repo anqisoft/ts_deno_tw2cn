c2ab4240eb54de6a75ec698d0153850004ecbda0
anqisoft <anqisoft@gmail.com>
2024-01-11 09:20:46 +0800

    update bat\bundle.bat, and call deno fmt, deno lint, deno bundle.

--- bat/deno/bundle.bat
+++ bat/deno/bundle.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 set deno_json_path=%cd%\..\..\deno.json
-cd ..\..\src
+cd ..\..\
 
-deno bundle -c %deno_json_path% --no-check index.ts ..\dist\index.js || pause
+deno bundle -c %deno_json_path% --no-check index.ts dist\index.js || pause
 
-cd ..\bat\
+cd bat\
 call compressJs.bat
